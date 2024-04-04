5d79a97e6f5d6f9a43a6fe05916a9df14edaae2b
rashaaz <rashaabdelaziz111@gmail.com>
2024-04-04 06:53:06 -0700

    add 3 file

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -1,42 +1,47 @@
 #!/usr/bin/python3
-"""This script initializes a Flask application for a web service.
+"""
+Starts a Flask web application.
 """
 from flask import Flask, render_template, url_for
 from models import storage
 import uuid
 
 
+# flask setup
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
+# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the storage.
+    """
+    Displays 4-hbnb.html template.
     """
     storage.close()
 
 
-@app.route('/100-hbnb/')
+@app.route('/4-hbnb/')
 def hbnb_filters(the_id=None):
-    """Render the HBNB filters.
     """
-    st_objs = storage.all('State').values()
-    sts = dict([state.name, state] for state in st_objs)
-    ams = storage.all('Amenity').values()
-    pls = storage.all('Place').values()
+    Displays 4-hbnb.html template.
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    return render_template('100-hbnb.html',
-                           sts=sts,
-                           ams=ams,
-                           pls=pls,
+    return render_template('4-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
                            users=users, cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
-    """Run the application.
     """
+    Displays 4-hbnb.html template."""
     app.run(host=host, port=port, threaded=True)
