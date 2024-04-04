f743dcefac88d5d316e444433b79fa08f4d0f367
rashaaz <rashaabdelaziz111@gmail.com>
2024-04-04 05:23:35 -0700

    modfy

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+"""
+from flask import Flask, render_template, url_for
+from models import storage
+import uuid
+
+
+# flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Displays 0-hbnb.html template.
+    """
+    storage.close()
+
+
+@app.route('/0-hbnb/')
+def hbnb_filters(the_id=None):
+    """
+    Displays 0-hbnb.html template.
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('0-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users, cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+    """
+    Displays 0-hbnb.html template."""
+    app.run(host=host, port=port)
